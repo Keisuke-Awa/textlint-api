cdb59da7d6fb8705bc65c34f4ea506284a141b4a
Keisuke-Awa <keisuke.awa.basket@gmail.com>
2021-07-29 23:14:22 +0900

    changed rule and using input file

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
 import { TextLintEngine } from "textlint";
+import fs from "fs";
+
 const engine = new TextLintEngine();
 
-engine.executeOnText("値を返却する").then((results) => {
+const testStr = fs.readFileSync("./test.txt", { encoding: "utf8" });
+
+engine.executeOnText(testStr).then((results) => {
   const messages = results[0].messages;
   messages.forEach((message) => {
     console.log(message);
   });
-  /*
-  [
-      {
-          id: "rule-name",
-          message:"lint message",
-          line: 1, // 1-based columns(TextLintMessage)
-          column:1 // 1-based columns(TextLintMessage)
-      }
-  ]
-   */
+
   if (engine.isErrorResults(results)) {
     const output = engine.formatResults(results);
     console.log(output);
