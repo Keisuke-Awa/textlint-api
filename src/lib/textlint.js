b25d4bdab5c5965cbfd5c7b03fc8f7935a9ef259
Keisuke-Awa <keisuke.awa.basket@gmail.com>
2021-08-09 09:42:10 +0900

    コメントアウトの削除

--- src/lib/textlint.js
+++ src/lib/textlint.js
@@ -5,7 +5,6 @@ const engine = new TextLintEngine();
 export async function proofreadText(text) {
   return new Promise((resolve, _reject) => {
     engine.executeOnText(text).then((results) => {
-      console.log("processing async func");
       const messages = results[0].messages;
       return resolve(messages);
     });
