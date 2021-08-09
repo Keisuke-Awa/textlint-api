8e85d492191e5f5c539d3686e9381f262e7ec8d0
Keisuke-Awa <keisuke.awa.basket@gmail.com>
2021-08-09 11:19:48 +0900

    疎通用のエンドポイントを追加

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 import { proofreadText } from "../../lib/textlint.js";
 
+router.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
 router.post("/proofread", (req, res) => {
   const text = req.body.text;
   proofreadText(text).then((resultMessages) => {
