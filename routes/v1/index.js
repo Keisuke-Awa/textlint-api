bef6767e0af0fb011baa190ec6f609aec849eaab
Keisuke-Awa <keisuke.awa.basket@gmail.com>
2021-08-09 22:09:41 +0900

    例外処理の追加

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -13,18 +13,25 @@ async function proofreadText(text) {
 
 router.get("/", (req, res) => {
   res.json({
-    message: "Hello World!",
+    message: "Hello World！！！！！",
   });
 });
 
 router.post("/proofread", (req, res) => {
   const text = req.body.text;
-  proofreadText(text).then((resultMessages) => {
-    res.json({
-      requestText: text,
-      resultMessages: resultMessages,
-    });
-  });
+  proofreadText(text).then(
+    (resultMessages) => {
+      res.json({
+        requestText: text,
+        resultMessages: resultMessages,
+      });
+    },
+    (error) => {
+      res.json({
+        message: error.message,
+      });
+    }
+  );
 });
 
 export default router;
